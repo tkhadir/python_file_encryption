550ec17e8c7b3cb1e23fc0bbf079da4dd0834c6c
tkhadir <tkhadir.dev@gmail.com>
2020-04-08 23:53:16 +0200

    sample block chain encryption script

---
+++ src/main.py
@@ -0,0 +1,67 @@
+import os
+from os import urandom
+import threading
+import blowfish
+
+content = []
+output = []
+
+class Reader(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        files = os.listdir('./../in/')
+        for f in files:
+            inFile = open('./../in/' + f, 'r')
+            content.append(inFile.read())
+            inFile.close()
+
+class Writer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        count = 0
+        for o in output:
+            outFile = open('./../out/out' + str(count), 'wb')
+            outFile.write(o)
+            outFile.close()
+            count+=1
+
+def getMsgToEncrypt(msg):
+    msgToEncrypt = msg
+    if len(msgToEncrypt) % 8 != 0:
+        toAdd = 8 - len(msgToEncrypt) % 8
+        count = 0
+        while (count < toAdd):
+            msgToEncrypt+="*"
+            count+=1
+    return msgToEncrypt.encode()
+
+def encrypt(msg):
+    iv = urandom(8)
+    cipher = blowfish.Cipher(b"test")
+    result = b"".join(cipher.encrypt_cbc_cts(getMsgToEncrypt(msg), iv))
+    print("iv="+ str(iv))
+    return result
+
+print("debut de traitement")
+thr = Reader()
+thr.start()
+thr.join()
+print("fin de lecture")
+
+print("debut de cryptage")
+for c in content:
+    output.append(encrypt(c))
+print("fin de cryptage")
+
+thw = Writer()
+thw.start()
+thw.join()
+print("fin d'ecriture")
+
+print("fin de traitement")
+
+
+
+
